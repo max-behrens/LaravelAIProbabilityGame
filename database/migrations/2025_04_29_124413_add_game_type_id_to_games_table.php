fe245b98505388482d504ef429ace57cf59e4d85
max.behrens <max.behrens@livepointuk.com>
2025-04-29 13:55:32 +0100

    Migration to add game_type_id commented out as create games migration should already add this field.

--- database/migrations/2025_04_29_124413_add_game_type_id_to_games_table.php
+++ database/migrations/2025_04_29_124413_add_game_type_id_to_games_table.php
@@ -8,17 +8,17 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::table('games', function (Blueprint $table) {
-            $table->unsignedBigInteger('game_type_id')->nullable();
-        });
+        // Schema::table('games', function (Blueprint $table) {
+        //     $table->unsignedBigInteger('game_type_id')->nullable();
+        // });
     }
     
     public function down()
     {
-        Schema::table('games', function (Blueprint $table) {
-            $table->dropForeign(['game_type_id']);
-            $table->dropColumn('game_type_id');
-        });
+        // Schema::table('games', function (Blueprint $table) {
+        //     $table->dropForeign(['game_type_id']);
+        //     $table->dropColumn('game_type_id');
+        // });
     }
     
 };
