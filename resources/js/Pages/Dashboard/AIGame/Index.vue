a41cabcbdb6480914c60804dc5aeca67266127fd
max_adm <max.behrens@dblx.co.uk>
2025-06-08 17:48:36 +0100

    Styling changed to dark mode tailwind for games pages.

--- resources/js/Pages/Dashboard/AIGame/Index.vue
+++ resources/js/Pages/Dashboard/AIGame/Index.vue
@@ -149,12 +149,12 @@ const enterGame = (gameId) => {
         </div>
 
         <div v-if="currentPageGames.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <div v-for="game in currentPageGames" :key="game.id" class="bg-white shadow-md rounded-lg p-6 flex flex-col justify-between">
-            <h2 class="text-xl text-gray-700 font-semibold mb-2">  {{ game.title }} {{ game.id }}: {{ game.game_type_name }}</h2>
-            <p class="text-gray-600 mb-2">
+          <div v-for="game in currentPageGames" :key="game.id" class="bg-gray-800 shadow-md rounded-lg p-6 flex flex-col justify-between">
+            <h2 class="text-xl text-white font-semibold mb-2">  {{ game.title }} {{ game.id }}: {{ game.game_type_name }}</h2>
+            <p class="text-gray-300 mb-2">
               {{ playersCount[game.id] || 0 }} / {{ game.max_players }} players
             </p>
-            <p class="text-xs text-gray-500 mb-2">
+            <p class="text-xs text-gray-400 mb-2">
               Status: {{ userInGames[game.id] ? 'You have joined' : 'Not joined' }}
             </p>
 
