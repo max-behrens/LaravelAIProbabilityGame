41b25d19092e369c1cb8f6509faae110fb5005c0
Max Behrens <maxbehrens@example.com>
2025-06-15 19:32:25 +0100

    CSRF Mismatch on 1st connection after login fixed.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+
+    <!-- This fixes the CSRF mismatch on 1st try after reload. -->
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
+
 
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
@@ -23,7 +33,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    @inertia
+    @inertia 
 </body>
 
 </html>
